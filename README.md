36021844fef4418970756911bb6789bd5fbf4cd9
shang yang <yangshang910215@gmail.com>
2024-08-27 22:45:39 -0400

    Simple routers

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Nodejs_Project
+
 Node js project
+Synchronous ---- Blocking
+single-thread for node.
+
+Asynchronous ---- Non-Blocking
+So use callback functions to excute features once the data is available(for instance, reading a very large file).
+following tasks can be excuted in the same thread and will not be blocked.
+
+so nodejs can handle asynchronous behaviour to implement non-blocking I/O model.
+-that is why use so many callback functions in Node.js
+-callbacks does not make it automatically asynchoronous.
+--** We can use ES6 Promises or ES8 Async/Await to solve callback hells**--
+
+\*Errow function does not get its own dis keyword
--- starter/index.js
+++ starter/index.js
@@ -1,18 +1,40 @@
-//Read data Sync
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+//Read data Sync
 // const contentRead = fs.readFileSync("./txt/read-this.txt", "utf-8");
 // console.log(contentRead);
 
 // fs.writeFileSync("./txt/writeSync", "writeSync");
 
 //Read data Async
-const readAsync = fs.readFile("./txt/input.txt", "utf-8", (err, data1) => {
-  if (err) return console.log(err);
-  fs.writeFile("./txt/writeAsyc", `${data1}`, "utf-8", (err) => {
-    if (err) console.log(err);
-    console.log("Successfully write a file");
-  });
-});
-console.log("will read file");
+// const readAsync = fs.readFile("./txt/input.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log(err);
+//   fs.writeFile("./txt/writeAsyc", `${data1}`, "utf-8", (err) => {
+//     if (err) console.log(err);
+//     console.log("Successfully write a file");
+//   });
+// });
+// console.log("will read file");
 
-//Write data Async jjj
+///////////////////////////////
+/////Server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  // console.log(pathName);
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is P ODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to port 8000");
+});
